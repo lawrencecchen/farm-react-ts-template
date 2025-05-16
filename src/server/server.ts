a4b0fed67e113467ef00f608530a07f4109063a0
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-05-16 16:12:26 -0700

    wip

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,6 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/api/hello", (c) => c.text("Hello Vite!"));
+app.get("/api/hello", (c) => c.text("Hello, world!"));
 
 export default app;
